1e304baeccfd3d94fb0b952294da4259d956c41c
pratik sharma <pratiksharma294@gmail.com>
2023-04-17 19:42:35 +0530

    fixed video problem

--- src/pages/International/VideoBG.js
+++ src/pages/International/VideoBG.js
@@ -1,10 +1,19 @@
 import React from "react";
 import vid from "../../assets/invid.mp4";
+import ReactPlayer from "react-player";
 import "./bg.scss";
 const VideoBG = () => {
   return (
     <div className="video-wrapper">
-      <video src={vid} autoPlay muted loop />
+      <ReactPlayer
+        url={vid}
+        playing={true}
+        loop={true}
+        muted={true}
+        controls={false}
+        width="100%"
+        height="100%"
+      />
       <div className="vid-text">
         <h1>We are your Study Abroad Co-Pilot!</h1>
       </div>
